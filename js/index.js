81a52e54140192d41237de3e3e0a346a00d4b643
santiago vassallo <santiagojvassallo@gmail.com>
2022-09-15 20:29:11 -0300

    domyeventos

--- js/index.js
+++ js/index.js
@@ -1,73 +1,74 @@
-class Producto {
-    constructor(id, nombre, precio, talle, disponibilidad) {
-        this.id = id
-        this.nombre = nombre
-        this.precio = parseInt(precio)
-        this.talle = talle
-        this.disponibilidad = disponibilidad
-    }
-    
-    habilitar(){
-        let error = "producto disponible"
-        if(this.disponibilidad == false){
-           this.disponibilidad = true
-        } else{
-          console.log(error)
-        }
-    }
 
-    desabibilitar (){
-        let error = "producto no disponible"
-        if(this.disponibilidad == true){
-            this.disponibilidad = false
-        } else {
-            console.log(error)
-        }
-    }
+
+ const productos =
+ [
+   {
+    id : 1,
+    imagen : "./imagenes/corderito blanco.jpg",
+    descripcion : "buzo-corderito",
+    nombre : "buzo corderito",
+    precio : 800,
+    talle : "1",
+    disponibilidad : true
+},
+
+{
+    id : 2,
+    imagen : "./imagenes/bichitos disfraz.jpg",
+    descripcion : "disfraz-perro",
+    nombre : "disfraz perro",
+    precio : 850,
+    talle : "1",
+    disponibilidad : true
+},
+
+{
+    id : 3,
+    imagen : "./imagenes/funda .jpeg",
+    descripcion : "funda-para-auto-con-lateral",
+    nombre : "funda para auto con lateral",
+    precio : 8000,
+    talle : "unico",
+    disponibilidad : false
 }
 
-const productos= [];
 
-function inventario (){
-productos.push(new Producto ("1","buzo corderito", 800, "1", true));
-productos.push(new Producto("2","buzo corderito", 1000, "2", false));
-productos.push(new Producto("3","buzo corderito", 1200, "3", true));
-productos.push(new Producto("4","disfraz perro ", 650, "1", true));
-productos.push(new Producto("5","disfraz perro", 850, "2", true));
-productos.push(new Producto("6","disfraz perro", 1000, "3", true));
-productos.push(new Producto("7","funda para auto con lateral", 8000, "unico", false));
-productos.push(new Producto("8","funda para auto sin lateral", 6000, "Único", true));
-productos.push(new Producto("9","correa para perro",900 , "Único", true));
-productos.push(new Producto("10","pretal", 1600, "Único", false));
-console.table(productos)
+
+ ]
+
+const eventos = () =>
+{
+    let buttons = document.querySelectorAll('.button');
+ for (const button of buttons) {
+    button.addEventListener('click', ()=>{
+        for (const prod of productos) {
+            if(prod.id == button.id){
+                alert('producto name:', prod.nombre)
+            }
+        }
+    })
  }
+}
 
 
-function compras (){
-    let compra = confirm("¿Desea comprar un producto?")
-    if (compra === true){
-    inventario()
-    let productoCompra = prompt("Ingrese el nombre del producto").toLocaleLowerCase();
-    let resultado = productos.filter(producto=> producto.nombre == productoCompra)
-    console.table(resultado)
-    let nuevoProducto = true
-    while(nuevoProducto){
-        nuevoProducto = confirm("desea agregar un nuevo producto" )
-        if (nuevoProducto == true) {
-            productoCompras = prompt ("ingre el nombre del producto")
-            let nuevoProducto = productos.filter(producto=> producto.nombre == productoCompras)
-            resultado = resultado.concat(nuevoProducto)
-            console.table(resultado)
-        }else {
-            alert("gracias por su compra")
-        } 
-        
+const cargaProducto = (productos) => 
+{
+    let container = document.querySelector('#container');
+    console.log('container:', container);
+    for (const product of productos) {
+        let div = document.createElement('div');
+        div.setAttribute('class','card');
+        div.innerHTML = `
+        <img class="imagen" src="${product.imagen}" alt="${product.descripcion}">
+        <h3 class="precio">$${product.precio}</h3>
+        <h4 class="nombre">${product.nombre}</h4>
+        <button class="button" id='${product.id}'> Agregar al carrito</button>
+        `;
+        container.appendChild(div);
     }
     
-}else {
-    console.log("gracias por su visita")
 }
- }
 
+cargaProducto(productos);
 
 
